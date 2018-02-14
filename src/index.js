7c3bd072dc720a79f2acc1d9c2256fb451f9f654
pg <pawelgabryelewicz@users.noreply.github.com>
2018-02-14 18:16:08 +0100

    TypeScript definitions, 1.2.1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-// G.assignbodyclasses 1.2.0.20171106
-// Copyright (C) 2013-2017 ConsidoNet Solutions / www.considonet.com
+// G.assignbodyclasses 1.2.1.20180214
+// Copyright (C) 2013-2018 ConsidoNet Solutions / www.considonet.com
 // Released under MIT Licence
 
 /*
 VERSION HISTORY
+1.2.1.20180214 @pg
++ TypeScript declarations
+
 1.2.0.20171106 @pg
 * Now acting as a separate npm module
 
