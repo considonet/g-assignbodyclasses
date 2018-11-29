96972de2f363e7a1bff43ba7d7d6679bd19b3f00
pg <pawelgabryelewicz@users.noreply.github.com>
2018-11-29 16:39:07 +0100

    Version bump in the source code

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-var _gEnv = require("@considonet/g-env");
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var _gEnv2 = _interopRequireDefault(_gEnv);
+var GEnv = _interopDefault(require('@considonet/g-env'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+// G.assignbodyclasses 2.1.0
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -21,34 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 
-  if (_gEnv2.default.isTouchDevice) {
+  if (GEnv.isTouchDevice) {
     addClass(document.body, "touchdevice");
   }
 
-  if (_gEnv2.default.browserInfo.isAndroidBrowser) {
+  if (GEnv.browserInfo.isAndroidBrowser) {
     addClass(document.body, "androidbrowser");
   }
-}); // G.assignbodyclasses 2.0.0
-// Copyright (C) 2013-2018 ConsidoNet Solutions / www.considonet.com
-// Released under MIT Licence
-
-/*
-VERSION HISTORY
-2.0.0 (20180718) @pg
-+ Switched to semver
-* Dist package now transpiled from ES6 (compatibility with building environments not transpiling node_modules)
-+ Source linted with tslint
-
-1.2.1.20180214 @pg
-+ TypeScript declarations
-
-1.2.0.20171106 @pg
-* Now acting as a separate npm module
-
-1.1.0 @pg
-+ ES6
-
-1.0.0 @pg
-+ Initial version
-
-*/
+});
